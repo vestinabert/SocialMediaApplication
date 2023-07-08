5c0970dd80ee563e8d522fa628c0631bc1340b7e
Vestina <vestina.bertasiute@gmail.com>
2023-07-08 15:29:57 +0300

    Attaching images to the post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:caption, :longitude, :latitude, :user_id, :allow_comments, :show_likes_count)
+      params.require(:post).permit(:caption, :longitude, :latitude, :user_id, :allow_comments, :show_likes_count, images: [])
     end
 end
