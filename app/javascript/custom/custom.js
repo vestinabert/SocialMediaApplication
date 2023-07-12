20c8e70847b371b750d2f4f49dd51a5683ab0ca3
Vestina <vestina.bertasiute@gmail.com>
2023-07-12 22:36:02 +0300

    Adding turbo load for the javascript

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -4,19 +4,24 @@
  // Import the plugin code
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
- FilePond.registerPlugin(
-    FilePondPluginImagePreview, 
-    FilePondPluginFileValidateType
-);
+document.addEventListener("turbo:load", loadFilePond);
 
- // Get a reference to the file input element
- const inputElement = document.querySelector("#post-images");
-
- // Create a FilePond instance
- const pond = FilePond.create(inputElement, {
-    credits: {},
-    storeAsFile:true,
-    allowMultiple: true,
-    allowReorder:true,
-    acceptedFileTypes: ['image/*'],
- });
+function loadFilePond() {
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview, 
+        FilePondPluginFileValidateType
+    );
+    
+     // Get a reference to the file input element
+     const inputElement = document.querySelector("#post-images");
+    
+     // Create a FilePond instance
+     const pond = FilePond.create(inputElement, {
+        credits: {},
+        storeAsFile:true,
+        allowMultiple: true,
+        allowReorder:true,
+        acceptedFileTypes: ['image/*'],
+     });
+}
+ 
