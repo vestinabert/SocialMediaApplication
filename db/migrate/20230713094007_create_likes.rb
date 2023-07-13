5bbc67c43512123ee136824cd7a0756ab06169ee
Vestina <vestina.bertasiute@gmail.com>
2023-07-13 12:48:32 +0300

    Enabling users to like a post just once

--- db/migrate/20230713094007_create_likes.rb
+++ db/migrate/20230713094007_create_likes.rb
@@ -4,6 +4,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
+      t.index [:user_id, :post_id], unique: true
       t.timestamps
     end
   end
