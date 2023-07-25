854cfeec1dde70e8edec8a7a8d944a22de569309
Vestina <vestina.bertasiute@gmail.com>
2023-07-25 13:55:39 +0300

    Adding follow states

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,11 @@ class User < ApplicationRecord
 
   # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
-  # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
+  has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
+  has_many :waiting_followings, through: :waiting_sent_requests, source: :followed
 
   def follow(user)
     Follow.create(follower: self, followed: user)
