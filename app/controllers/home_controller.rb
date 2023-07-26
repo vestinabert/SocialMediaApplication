f1faecb5f27c88427d4a87f464050374257707f2
Vestina <vestina.bertasiute@gmail.com>
2023-07-26 14:38:59 +0300

    Adding personalized feed to every user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
   def index
   end
   private
-  def set_feeds
-    @feeds = Post.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
-  end
+    def set_feeds
+      @feeds = Post.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
+    end
 
   def set_suggestions
     @suggestions = [current_user.followers]
